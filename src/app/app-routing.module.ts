db4767919318af49a0b70578b4e35f0aa1a52c85
anuragpatle <anurag.ap6@gmail.com>
2020-02-13 14:58:36 +0530

    Removed unwanted imports from routing file
    Removed unwanted imports from app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BackbtnComponent } from './backbtn/backbtn.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
 
 const routes: Routes = [
